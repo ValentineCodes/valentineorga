323c7a5cf9e8f9d9c5fbbb3e264cd211c1f5c9b6
Valentine Orga <valentineorga@gmail.com>
2024-03-04 13:21:25 +0100

    design update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-import { CiMail, CiLocationOn } from "react-icons/ci";
 import { GoPackage } from "react-icons/go";
-import { IoIosLink } from "react-icons/io";
+import { FiPhoneCall } from "react-icons/fi";
 import { BsDownload } from "react-icons/bs";
+import { GrLocation } from "react-icons/gr";
+import { LuMail } from "react-icons/lu";
 import Link from 'next/link';
 
 type Props = {}
@@ -18,20 +19,20 @@ export default function Hero({ }: Props) {
 
                 <div className='mt-4 space-y-3 inline-block'>
                     <p className='flex items-center text-sm space-x-3 text-gray-400'>
-                        <CiMail color='#15f7d6' />
+                        <LuMail color='#15f7d6' />
                         <span>Valentineorga@gmail.com</span>
                     </p>
                     <p className='flex items-center text-sm space-x-3 text-gray-400'>
-                        <CiLocationOn color='#15f7d6' />
+                        <FiPhoneCall color='#15f7d6' />
+                        <span>+2348140715721</span>
+                    </p>
+                    <p className='flex items-center text-sm space-x-3 text-gray-400'>
+                        <GrLocation color='#15f7d6' />
                         <span>Nigeria</span>
                     </p>
                     <p className='flex items-center text-sm space-x-3 text-gray-400'>
                         <GoPackage color='#15f7d6' />
                         <span>Full-time / Freelancer</span>
-                    </p>
-                    <p className='flex items-center text-sm space-x-3 text-gray-400'>
-                        <IoIosLink color='#15f7d6' />
-                        <span>www.valentineorga.com</span>
                     </p>
 
                     <div className='flex flex-row items-center space-x-4 text-sm font-light text-black'>
@@ -60,7 +61,7 @@ export default function Hero({ }: Props) {
                 <p className='ml-4 text-sm md:text-md text-gray-400 max-w-lg'>I help businesses grow by crafting out great user experiences. If you're looking for developer that loves to get stuff done.</p>
                 <p className='text-[#15f7d6] font-thin text-sm'>{'</p>'}</p>
 
-                <Link href="#contact" className='flex flex-row items-center text-[#15f7d6] text-3xl mt-4 ml-4'>Let's Talk <CiMail className='ml-2' /></Link>
+                <Link href="#contact" className='flex flex-row items-center text-[#15f7d6] text-3xl mt-4 ml-4'>Let's Talk <span className='bg-[#43454e] p-2 ml-2 rounded-full'><LuMail className='text-[1rem]' /></span></Link>
             </div>
         </div>
     )
